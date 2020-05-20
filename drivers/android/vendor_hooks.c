e54e2745725988483d1be3813b0d8f01c9b8704d
Yun Hsiang <yun.hsiang@mediatek.com>
2020-05-20 10:31:35 +0800

    ANDROID: sched: add restrict vendor hook to modify task placement policy in EAS
    For modifying task placement policy, we add the hook on the top of
    select_task_rq_fair(). It allows us to modify wakeup/fork/exec balance
    paths.
    
    Bug: 165660734
    Signed-off-by: Yun Hsiang <yun.hsiang@mediatek.com>
    Change-Id: I80ff870453472e183ab2aae7381bff91e49ae296
    (cherry picked from commit a1fc1fba460be48dff2484b438888ede557930d4)

--- drivers/android/vendor_hooks.c
+++ drivers/android/vendor_hooks.c
@@ -8,9 +8,22 @@
 
 #define CREATE_TRACE_POINTS
 #include <trace/hooks/vendor_hooks.h>
+#include <trace/hooks/sched.h>
+#include <trace/hooks/dtask.h>
+#include <trace/hooks/net.h>
 
 /*
  * Export tracepoints that act as a bare tracehook (ie: have no trace event
  * associated with them) to allow external modules to probe them.
  */
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_rvh_select_task_rq_fair);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_mutex_wait_start);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_mutex_wait_finish);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_rwsem_read_wait_start);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_rwsem_read_wait_finish);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_rwsem_write_wait_start);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_rwsem_write_wait_finish);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_sched_show_task);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_ptype_head);
+EXPORT_TRACEPOINT_SYMBOL_GPL(android_vh_kfree_skb);
 
