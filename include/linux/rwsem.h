f03c8fa3e4940c301dc0b3724f0fa7b38154d2b6
JianMin Liu <jian-min.liu@mediatek.com>
2020-07-21 13:36:22 +0800

    ANDROID: rwsem: Add vendor hook to the rw-semaphore
      - Add the hook to apply vendor's performance tune for owner
        of rwsem.
    
      - Add the hook for the waiter list of rwsem to allow
        vendor perform waiting queue enhancement
    
      - ANDROID_VENDOR_DATA added to rw_semaphore
    
    Bug: 161400830
    
    Signed-off-by: JianMin Liu <jian-min.liu@mediatek.com>
    Change-Id: I007a5e26f3db2adaeaf4e5ccea414ce7abfa83b8

--- include/linux/rwsem.h
+++ include/linux/rwsem.h
@@ -19,6 +19,7 @@
 #ifdef CONFIG_RWSEM_SPIN_ON_OWNER
 #include <linux/osq_lock.h>
 #endif
+#include <linux/android_vendor.h>
 
 struct rw_semaphore;
 
@@ -46,6 +47,7 @@ struct rw_semaphore {
 	/* count for waiters preempt to queue in wait list */
 	long m_count;
 #endif
+	ANDROID_VENDOR_DATA(1);
 };
 
 /*
