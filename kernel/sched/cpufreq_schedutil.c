91003f13fec504b144d0c46e84cf7777f0341238
skye-pa1n <sonicgames772@gmail.com>
2025-07-18 19:12:46 -0300

    not: HZ=1000

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -827,7 +827,7 @@ static int sugov_init(struct cpufreq_policy *policy)
 		goto stop_kthread;
 	}
 
-	tunables->rate_limit_us = 1500;
+	tunables->rate_limit_us = 0;
 
 	policy->governor_data = sg_policy;
 	sg_policy->tunables = tunables;
